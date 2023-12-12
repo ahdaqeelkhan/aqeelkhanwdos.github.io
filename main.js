9ecd45f59e7ea770bb8367461476bc489e0027af
ahdaqeelkhan <144414970+ahdaqeelkhan@users.noreply.github.com>
2023-12-12 14:54:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// Check if the browser supports service workers
+if ('serviceWorker' in navigator) {
+    // Register the service worker
+    navigator.serviceWorker.register('/service-worker.js')
+        .then(function(registration) {
+            console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch(function(error) {
+            console.error('Service Worker registration failed:', error);
+        });
+}
